17fa6d5927b0649bedd38590ed6655ba6f98bf2d
Beyler55 <baylarss@code.edu.az>
2022-06-09 18:48:38 +0400

    custom arraylist

---
+++ CustomArraylist/Arraylist.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace CustomArraylist
+{
+    public class Arraylist
+    {
+        private object[] _arr;
+        public int Capacity { get; set; }
+        public int Count { get; set; }
+        public Arraylist(int capacity)
+        {
+            Capacity = capacity;
+            _arr = new object[capacity];
+        }
+        int index = 0;
+        public void Add(object value)
+        {
+            if (Count < Capacity)
+            {
+                Array.Resize(ref _arr, Capacity);
+                _arr[index] = value;
+                index++;
+            }
+            else if (Count > Capacity || Count == Capacity)
+            {
+                Capacity = Capacity * 2;
+                Array.Resize(ref _arr, Capacity);
+                _arr[index] = value;
+                index++;
+            }
+            Count++;
+        }
+
+        public void Remove(object value)
+        {
+            int valindex = Array.IndexOf(_arr, value);
+            int index = 0;
+            while (index != valindex)
+            {
+                index++;
+            }
+            while (index < _arr.Length - 1)
+            {
+                _arr[index] = _arr[index + 1];
+                index++;
+            }
+            valindex--;
+        }
+        public void RemoveAt(int removedindex)
+        {
+            int index = 0;
+            while (index != removedindex)
+            {
+                index++;
+            }
+            while (index < _arr.Length - 1)
+            {
+                _arr[index] = _arr[index + 1];
+                index++;
+            }
+            removedindex--;
+        }
+        public  void TrimToSize()
+        {
+            
+        }
+        public void AddRange(object[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Array.Resize(ref _arr, _arr.Length + 1);
+                _arr[_arr.Length-1] = array[i];
+            }
+
+        }
+        public void GetAll()  //print all elements
+        {
+            for (int i = 0; i <_arr.Length; i++)
+            {
+                if (_arr[i]!=null)
+                {
+                    Console.WriteLine(_arr[i]);
+                }
+            }
+        }
+    }
+}
---
+++ CustomArraylist/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace CustomArraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arraylist array = new Arraylist(5);
+            array.Add(7);  //0---->7
+            array.Add(4);  //1---->12
+            array.Add(5);  //1---->12
+            array.Add(8);  //1---->12
+           object[] arr = { 1, 2, 3, 4, 5, 6 };
+
+           
+            
+            
+        }
+        
+    }
+    
+}
